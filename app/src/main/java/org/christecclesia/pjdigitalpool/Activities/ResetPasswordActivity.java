c7408b521be1add64b395ef8eb3699e3f0555aa0
zatana <annodankyikwaku@gmail.com>
2020-08-12 21:57:28 +0000

    Partially completed xml designs for signup, login and reset activities

---
+++ app/src/main/java/org/christecclesia/pjdigitalpool/Activities/ResetPasswordActivity.java
@@ -0,0 +1,16 @@
+package org.christecclesia.pjdigitalpool.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.christecclesia.pjdigitalpool.R;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
