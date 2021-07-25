de8cfa6dc6e2b5158b68098d048ce9800f955b8b
zatana <annodankyikwaku@gmail.com>
2021-07-25 20:09:30 +0000

    Updated christwitness url

--- app/src/main/java/org/christecclesia/pjdigitalpool/Fragments/WitnessFragment.java
+++ app/src/main/java/org/christecclesia/pjdigitalpool/Fragments/WitnessFragment.java
@@ -101,7 +101,7 @@ public class WitnessFragment extends Fragment implements View.OnClickListener {
             //ntent intent = new Intent(getActivity().getApplicationContext(), PartnershipActivity.class);
             //startActivity(intent);
         } else if(view.getId() == m_christwitness_holder_constraintlayout.getId()){
-            startActivity(getOpenFacebookIntent(getActivity().getPackageManager(), "https://www.facebook.com/thegloriouschurch11"));
+            startActivity(getOpenFacebookIntent(getActivity().getPackageManager(), "https://www.facebook.com/Christecclesia"));
         } else if(view.getId() == m_holygen_holder_constraintlayout.getId()){
             startActivity(getOpenFacebookIntent(getActivity().getPackageManager(), "https://www.facebook.com/theHoly.Generation20"));
         } else if(view.getId() == m_impacttrain_holder_constraintlayout.getId()){
